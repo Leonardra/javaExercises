88c15ccf0e57c7c5ed00311ceb8ea387e01338b0
Oluwatobi Jolayemi <jolayemi.tobi@gmail.com>
2021-05-16 10:41:01 +1100

    Second Commit:Second and Third functionality "Bike can decelerate and accelerate"

--- src/com/incluctab/tddClasses/chapterThree/Bike.java
+++ src/com/incluctab/tddClasses/chapterThree/Bike.java
@@ -4,20 +4,72 @@ public class Bike {
 
     private boolean isOn;
     private int speedInKmPerHour;
+    private int currentGearNumber;
 
     public void setOn(boolean bikeIgnition) {
         isOn = bikeIgnition;
+        if(this.isOn){
+            currentGearNumber = 1;
+        }
+        else{
+            currentGearNumber = 0;
+        }
+
     }
 
-    public Object bikeIsOn(boolean bikeIsOn) {
+    public Object bikeIsOn(){
         return isOn;
     }
 
     public void setSpeedInKmPerHour(int currentSpeed) {
-        speedInKmPerHour = currentSpeed;
+        if (isOn) {
+            speedInKmPerHour = currentSpeed;
+        }
     }
 
     public int getSpeedInKmPerHour() {
         return speedInKmPerHour;
+    }
+
+    public void accelerate(){
+        if(speedInKmPerHour >= 1 && speedInKmPerHour < 20){
+            currentGearNumber = 1;
+        }
+        if(speedInKmPerHour >= 20 && speedInKmPerHour < 40){
+            currentGearNumber = 2;
+        }
+        if(speedInKmPerHour >= 40 && speedInKmPerHour < 60){
+            currentGearNumber = 3;
+        }
+        if(speedInKmPerHour >= 60 && speedInKmPerHour < 80){
+            currentGearNumber = 4;
+        }
+
+        speedInKmPerHour = speedInKmPerHour + currentGearNumber;
+    }
+
+    public int getCurrentGearNumber() {
+        return currentGearNumber;
+    }
+
+    public void setCurrentGearNumber(int currentGearNumber) {
+        this.currentGearNumber = currentGearNumber;
+    }
+
+    public void decelerate() {
+        if(speedInKmPerHour >= 1 && speedInKmPerHour < 20){
+            currentGearNumber = 1;
+        }
+        if(speedInKmPerHour >= 20 && speedInKmPerHour < 40){
+            currentGearNumber = 2;
+        }
+        if(speedInKmPerHour >= 40 && speedInKmPerHour < 60){
+            currentGearNumber = 3;
+        }
+        if(speedInKmPerHour >= 60 && speedInKmPerHour < 80){
+            currentGearNumber = 4;
+        }
+
+        speedInKmPerHour = speedInKmPerHour - currentGearNumber;
     }
 }
--- src/com/incluctab/tddClasses/chapterThree/BikeTest.java
+++ src/com/incluctab/tddClasses/chapterThree/BikeTest.java
@@ -5,20 +5,66 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/*
+Requirements
+1) Bike should be changed speed till its on.
+2) Bike can accelerate and decelerate
+2) Gears should change automatically:
+ - If Speed reaches 20, current gear number changes from 1 to 2
+ - If Speed reaches 40, current gear number changes from 2 to 3
+ - If speed reaches 60, current gear number changes from 3 to 4
+ - If speed reaches 80, bike shows warning sign.
+3)Speed should be in the multiples of the current gear number.
+4) Bike should have fuel gauge.
+
+ */
+
 public class BikeTest {
+
+    private Bike newBike;
+
+    @BeforeEach
+    public void startTheTestWithThis(){
+        newBike = new Bike();
+    }
 
 
     @Test
     public void testThatBikeDoesNotSetSpeedWhenOff(){
-        //when
-        Bike newBike = new Bike();
         //given
         newBike.setOn(false);
-        assertFalse((Boolean) newBike.bikeIsOn(false));
+        assertFalse((Boolean) newBike.bikeIsOn());
         newBike.setSpeedInKmPerHour(0);
         //check that
         assertEquals(0, newBike.getSpeedInKmPerHour());
     }
 
+    @Test
+    public void testThatBikeCanAccelerate(){
+        //given
+        newBike.setOn(true);
+        assertTrue((Boolean) newBike.bikeIsOn());
+        newBike.setSpeedInKmPerHour(15);
+        newBike.setCurrentGearNumber(1);
+        assertEquals(1, newBike.getCurrentGearNumber());
+        //when
+        newBike.setSpeedInKmPerHour(20);
+        newBike.accelerate();
+        //check that
+        assertEquals(22, newBike.getSpeedInKmPerHour());
+        assertEquals(2, newBike.getCurrentGearNumber());
+    }
+    @Test
+    public void testThatBikeCanDecelerate(){
+        //when
+        newBike.setOn(true);
+        newBike.setSpeedInKmPerHour(22);
+        newBike.accelerate();
+        assertEquals(2, newBike.getCurrentGearNumber());
+        //given
+        newBike.setSpeedInKmPerHour(15);
+        newBike.decelerate();
+        assertEquals(1, newBike.getCurrentGearNumber());
+    }
 
 }
