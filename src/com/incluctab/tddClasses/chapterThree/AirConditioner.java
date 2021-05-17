becadd79566544f99c98c648b0b24aa78e83fa0d
Oluwatobi Jolayemi <jolayemi.tobi@gmail.com>
2021-05-17 19:23:01 +1100

    Air Condition Test and Main Class

---
+++ src/com/incluctab/tddClasses/chapterThree/AirConditioner.java
@@ -0,0 +1,45 @@
+package com.incluctab.tddClasses.chapterThree;
+
+public class AirConditioner {
+
+    private String productName;
+    private boolean isOn;
+    private int temperature;
+
+    public AirConditioner (String brandName){
+        productName = brandName;
+    }
+
+    public void setOn(boolean ignition) {
+        isOn = ignition;
+
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+
+    public void decreaseTemperature() {
+
+        if(isOn && temperature > 16){
+            temperature = temperature - 1;
+        }
+    }
+
+    public void setTemperature(int currentTemperature) {
+            temperature = currentTemperature;
+
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void increaseTemperature() {
+
+        if(isOn && temperature < 30) {
+            temperature = temperature + 1;
+        }
+    }
+}
---
+++ src/com/incluctab/tddClasses/chapterThree/AirConditionerTest.java
@@ -0,0 +1,68 @@
+package com.incluctab.tddClasses.chapterThree;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AirConditionerTest {
+    private AirConditioner myAirConditioner;
+    @BeforeEach
+    public void startEveryMethodWithThis(){
+        myAirConditioner = new AirConditioner("Samsung");
+    }
+    @Test
+    public void testThatAirConditionerCanSwitchOn(){
+        myAirConditioner.setOn(false);
+        assertFalse(myAirConditioner.isOn());
+        myAirConditioner.setOn(true);
+        assertTrue(myAirConditioner.isOn());
+    }
+
+    @Test
+    public void testThatAirConditionerCanDecreaseTemperature(){
+        myAirConditioner.setOn(true);
+        myAirConditioner.setTemperature(30);
+        myAirConditioner.decreaseTemperature();
+        assertEquals(29, myAirConditioner.getTemperature());
+
+    }
+    @Test
+    public void testThatTemperatureCannotBeDecreasedBelowOne(){
+        myAirConditioner.setOn(true);
+        myAirConditioner.setTemperature(20);
+        myAirConditioner.decreaseTemperature();
+        assertEquals(19, myAirConditioner.getTemperature());
+    }
+    @Test
+    public void testThatAirConditionCanIncreaseTemperature(){
+        myAirConditioner.setOn(true);
+        myAirConditioner.setTemperature(19);
+        myAirConditioner.increaseTemperature();
+        assertEquals(20, myAirConditioner.getTemperature());
+
+    }
+    @Test
+    public void testThatAirConditionerCannotIncreaseAboveThirty(){
+        myAirConditioner.setOn(true);
+        myAirConditioner.setTemperature(30);
+        myAirConditioner.increaseTemperature();
+        assertEquals(30, myAirConditioner.getTemperature());
+
+    }
+    @Test
+    public void testThatAirConditionWillNotDecreaseWhenOff(){
+        myAirConditioner.setOn(false);
+        myAirConditioner.setTemperature(30);
+        myAirConditioner.decreaseTemperature();
+        assertEquals(30, myAirConditioner.getTemperature());
+    }
+    @Test
+    public void testThatAirConditionWillNotIncreaseWhenOff(){
+        myAirConditioner.setOn(false);
+        myAirConditioner.setTemperature(29);
+        myAirConditioner.increaseTemperature();
+        assertEquals(29, myAirConditioner.getTemperature());
+    }
+
+}
